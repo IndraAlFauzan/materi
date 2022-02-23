ac1bf2e98341aefa28252e0f1140eb41e85b5b5a
IndraAlFauzan <39118928+IndraAlFauzan@users.noreply.github.com>
2022-02-23 12:35:31 +0700

    Add project files.

---
+++ materi/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace materi
+{
+
+    class Rumus
+    {
+        //deklarasi variable
+        private double panjang, lebar, luas;
+  
+        public void input()
+        {
+            Console.Write("Masukan Panjang: ");
+            panjang = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Masukan Lebar: ");
+            lebar = Convert.ToDouble(Console.ReadLine());
+        }
+
+        public void prosesdata()
+        {
+            luas =  panjang * lebar;
+        }
+        public void tampilData()
+        {
+            Console.WriteLine("Panjang={0} Lebar = {1} luas = {2}", panjang, lebar, luas);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Rumus rumus = new Rumus();
+            rumus.input();
+            rumus.prosesdata();
+            rumus.tampilData();
+
+            Console.ReadKey();
+        }
+    }
+}
